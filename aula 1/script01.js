be877909689a7896bfa53004f8196354aafe40b2
Diana Braite <ddianabbraite99@gmail.com>
2024-08-06 14:26:41 -0300

    coloca os exercicios das olimpiadas no repositorio

---
+++ aula 1/script01.js
@@ -0,0 +1,7 @@
+var prata = 8.75;
+var ouro = 2 * prata;
+var bronze = prata + 5;
+
+console.log('O País X tem' + prata + 'medalhas de prata');
+console.log('O País X tem' + bronze + 'medalhas de bronze');
+console.log('O País X tem' + ouro + 'medalhas de ouro')
---
+++ aula 1/script02.js
@@ -0,0 +1,28 @@
+console.log('Escolha o país: ')
+
+var Brasil = 3;
+var PrataBrasil = 1;
+var BronzeBrasil = 2;
+var OuroBrasil = 0;
+
+var Japão = 13;
+var PrataJapao = 2;
+var BronzeJapao = 4;
+var OuroJapa = 7;
+
+var China = 14;
+var PrataChina = 6;
+var BronzeChina = 2;
+var OuroChina = 6;
+
+var França = 17;
+var PrataFrança = 8;
+var BronzeFrança = 4;
+var OuroFrança = 5;
+
+
+
+
+
+
+
